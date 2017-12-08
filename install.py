14143e446d3c5d8a09eaf77af629f1091039a955
渡部真伍 <s1170087@gmail.com>
2017-12-08 23:53:50 +0900

    modify: install.py to check whether directory exists or not

--- install.py
+++ install.py
@@ -6,26 +6,32 @@ import os
 HOME = os.getenv('HOME')
 CONFIG_ROOT = os.path.join(HOME, 'dotfiles')
 
-FILES = ['.zshrc', '.vimrc', '.tmux.conf', '.ghci', '.iex.exs']
-DIRS = ['.zsh', '.vim', '.config/nvim', '.git-subcommands']
-PATH_LIST = FILES + DIRS
+DIRS = ['.zsh', '.vim', '.config/nvim', '.cache', '.local/share', '.tmux/plugins']
+LINK_LIST = ['.zshrc', '.vimrc', '.tmux.conf', '.ghci', '.iex.exs', '.cache/dein', '.tmux/plugins/tpm', '.git-subcommands']
 
 def remove_files():
-    home_files = [os.path.join(HOME, fn) for fn in PATH_LIST]
+    home_files = [os.path.join(HOME, fn) for fn in LINK_LIST]
     home_files = [path for path in home_files if os.path.exists(path)]
 
     for path in home_files:
         os.remove(path)
 
 def link_files():
-    home_files = [os.path.join(HOME, fn) for fn in PATH_LIST]
-    dot_files = [os.path.join(CONFIG_ROOT, fn) for fn in PATH_LIST]
+    home_files = [os.path.join(HOME, fn) for fn in LINK_LIST]
+    dot_files = [os.path.join(CONFIG_ROOT, fn) for fn in LINK_LIST]
 
     for home_file, dot_file in zip(home_files, dot_files):
         print('link\t{}\tto\t{}'.format(dot_file, home_file))
         os.symlink(dot_file, home_file)
 
+def make_dirs():
+    pathes = [os.path.join(HOME, d) for d in DIRS]
+    pathes = [p for p in pathes if not os.path.exists(p)]
+    for path in pathes:
+        os.makedirs(path)
+
 def main():
+    make_dirs()
     remove_files()
     link_files()
 
