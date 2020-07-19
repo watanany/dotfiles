9dd01621b082c830f2ec7674a2792eb621815df2
watanany <s1170087@gmail.com>
2020-07-19 13:30:02 +0900

    change: remove +dante option

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -125,8 +125,7 @@
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
-       ;;(haskell +lsp)    ; a language that's lazier than I am
-       (haskell +dante +lsp)    ; a language that's lazier than I am
+       (haskell +lsp)      ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        json              ; At least it ain't XML
