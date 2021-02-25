447bead3cee3ce2edbfad83f86ed87370f2343b4
watanany <s1170087@gmail.com>
2021-02-25 10:58:46 +0900

    change: user profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,11 +4,20 @@
             [lein-pprint "1.3.2"]
             ]
   :dependencies [[org.clojure/tools.namespace "1.1.0"]
+                 [org.clojure/tools.trace "0.7.11"]
                  [org.clojure/data.json "1.0.0"]
-                 [hashp "0.2.0"]
                  [clj-http "3.12.0"]
+                 [clj-time "0.15.2"]
+                 [rm-hull/infix "0.3.3"]
                  [rhizome "0.2.9"]
                  ]
   :injections [(require '[clojure.tools.namespace.repl :refer [refresh]])
-               (require 'hashp.core)
+               (require '[clojure.tools.trace :refer :all])
+               (require '[clojure.set :as set])
+               (require '[clojure.string :as str])
+               (require '[clojure.data.json :as json])
+               (require '[clj-time.core :as time])
+               (require '[clj-time.format :as time-format])
+               (require '[clj-time.local :as time-local])
+               (require '[infix.macros :refer [infix $=]])
                ]}}
