d90231a49bbd1f02e06c47aef3b10b22336fd2a1
Watanabe Shingo <shingo@dreamstock.co.jp>
2021-08-06 12:30:13 +0900

    Add upgrade_doom.py

--- upgrade_doom.py
+++ upgrade_doom.py
@@ -2,6 +2,7 @@
 import shutil
 import tempfile
 import os.path
+from urllib.request import urlopen
 from subprocess import Popen, PIPE
 
 EMACS_DIR = os.path.expanduser("~/.emacs.d")
@@ -48,12 +49,23 @@ def restore(emacs_dir):
     )
 
 
+def install_plantuml():
+    url = "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"
+    path = os.path.join(EMACS_DIR, ".local/etc/plantuml.jar")
+    with open(path, "wb") as writer, urlopen(url) as res:
+        writer.write(res.read())
+
+
 def main():
     emacs_dir = evacuate_emacs_dir()
-    install_doom()
-    if emacs_dir is not None:
-        restore(emacs_dir)
-    print(f"Original {EMACS_DIR} is moved to {emacs_dir}")
+    try:
+        install_doom()
+        install_plantuml()
+        if emacs_dir is not None:
+            restore(emacs_dir)
+    finally:
+        print("\n\n", end="")
+        print(f"[Info] Original {EMACS_DIR} is moved to {emacs_dir}")
 
 
 if __name__ == "__main__":
