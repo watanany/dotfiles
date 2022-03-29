5ceccd29fc55432f7f6d4456cb563775943804e7
watanany <s1170087@gmail.com>
2022-03-29 20:37:51 +0900

    Add fish config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,33 @@
+# Greeting messageを変更する
+# cf. <https://github.com/fish-shell/fish-shell/issues/2346#issuecomment-136361397>
+function fish_greeting
+    set_color normal; echo
+    set_color red; echo -n "*"
+    set_color normal; echo -n " "
+    set_color yellow; echo -n "*"
+    set_color normal; echo -n " "
+    set_color purple; echo -n "*"
+    set_color normal; echo -n " "
+    set_color cyan; echo -n "*"
+    set_color normal; echo -n " "
+    set_color normal; echo -n "Hello World"
+    set_color normal; echo -n " "
+    set_color red; echo -n "*"
+    set_color normal; echo -n " "
+    set_color yellow; echo -n "*"
+    set_color normal; echo -n " "
+    set_color purple; echo -n "*"
+    set_color normal; echo -n " "
+    set_color cyan; echo -n "*"
+    set_color normal; echo -n " "
+    set_color normal; echo
+    set_color normal; echo
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    export FZF_DEFAULT_OPTS='--no-height --layout=reverse'
+
+    set -gx PATH ~/.emacs.d/bin $PATH
+    set -gx PATH ~/.local/bin $PATH
+end
