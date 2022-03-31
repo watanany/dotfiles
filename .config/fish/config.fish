38cc9cf403b31c3de12915c079e567799beecfb6
Watanabe Shingo <shingo@dreamstock.co.jp>
2022-03-31 18:32:25 +0900

    Fix export syntax

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    export FZF_DEFAULT_OPTS='--layout=reverse'
+    set -gx FZF_DEFAULT_OPTS "--layout=reverse"
 
     set -gx PATH "$HOME/.emacs.d/bin" $PATH
     set -gx PATH "$HOME/.local/bin" $PATH
