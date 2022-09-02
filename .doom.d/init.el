cb502c01decaa0a4a8696233a10d94c791ccd95c
watanany <watanabe.s@note.jp>
2022-09-02 09:59:08 +0900

    Add purescript module with lsp support

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -150,7 +150,8 @@
        org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
-       purescript        ; javascript, but functional
+       ;;purescript        ; javascript, but functional
+       (purescript +lsp)        ; javascript, but functional
        python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
