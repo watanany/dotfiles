9108c3b8a82479f1c5f20153f40f7e59622f9aa7
watanany <s1170087@gmail.com>
2023-03-18 14:09:04 +0900

    Change neovim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,22 +1,73 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require("plugins")` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 -- vim.cmd [[
 --   packadd packer.nvim
 -- ]]
 
-return require('packer').startup(function(use)
+local function ensure_packer()
+  local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+    vim.cmd [[
+      packadd packer.nvim
+    ]]
+    return true
+  else
+    return false
+  end
+end
+
+local packer_bootstrap = ensure_packer()
+
+local function startup(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
   -- Configurations for Nvim LSP
-  use 'neovim/nvim-lspconfig'
+  use "neovim/nvim-lspconfig"
 
   -- カラースキーム
-  use 'w0ng/vim-hybrid'
+  use "w0ng/vim-hybrid"
 
   -- カーソル移動加速プラグイン
-  use 'rainbowhxch/accelerated-jk.nvim'
+  use "rainbowhxch/accelerated-jk.nvim"
 
   -- f + 一文字で検索
-  use 'rhysd/clever-f.vim'
-end)
+  use "rhysd/clever-f.vim"
+
+  -- セッション管理
+  use "rmagatti/auto-session"
+
+  -- ファイラー
+  use "lambdalisue/fern.vim"
+
+  -- fzf本体(CLI)
+  use { "junegunn/fzf", run = ":call fzf#install()" }
+  -- vim用のfzfプラグイン
+  use "junegunn/fzf.vim"
+
+  use { "akinsho/toggleterm.nvim", tag = "*", config = function()
+    require("toggleterm").setup()
+  end }
+
+  -- CSVを見やすく色付け
+  use "mechatroner/rainbow_csv"
+
+  -- packerがインストールされた初回のみPackerSyncを行う
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+end
+
+local config = {
+  display = {
+    -- :PackerSyncとかするときに出るディスプレイの設定
+    -- <https://github.com/wbthomason/packer.nvim#using-a-floating-window>
+    open_fn = function()
+      return require("packer.util").float({ border = "single" })
+    end
+  },
+}
+
+return require("packer").startup({ startup, config = config })
