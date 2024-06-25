f4cd03691ce6f1ba1e3f762f34cf6cc46b28a391
watanany <s1170087@gmail.com>
2024-06-25 15:19:59 +0900

    Update ipython startup

--- .ipython/profile_default/startup/00-first.py
+++ .ipython/profile_default/startup/00-first.py
@@ -1,7 +1,12 @@
+import sys
+import os
 import csv
+import json
+import yaml
 from os.path import expanduser
-from datetime import datetime
+from datetime import datetime, timedelta
 from glob import glob
+from pathlib import Path
 
 try:
     import pandas as pd
@@ -9,8 +14,12 @@ except ImportError:
     pass
 
 try:
-    from see import see
+    import polars as pl
 except ImportError:
     pass
 
+try:
+    from see import see
+except ImportError:
+    pass
 
