d782b469fda842ff4665c9ff22cdaf5ce5c7de86
watanany <s1170087@gmail.com>
2025-02-08 16:26:06 +0900

    Add opam config to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -124,8 +124,20 @@ if status is-interactive
 
     #;;; fzf
     fzf --fish | source
+
+    # BEGIN opam configuration
+    # This is useful if you're using opam as it adds:
+    #   - the correct directories to the PATH
+    #   - auto-completion for the opam binary
+    # This section can be safely removed at any time if needed.
+    if test -r "$HOME/.opam/opam-init/init.fish"
+        source '/Users/watanabe.s/.opam/opam-init/init.fish' > /dev/null 2> /dev/null
+    end
+    # END opam configuration
 end
 
 if test -f $(status dirname)/local_config.fish
     source $(status dirname)/local_config.fish
 end
+
+
