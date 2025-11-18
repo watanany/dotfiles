65927219a2a4d790623c61089696347695684ee0
watanany <watanabe.s@note.jp>
2025-11-18 12:12:35 +0900

    Update neovim configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -199,7 +199,7 @@ end, { noremap = true, silent = true })
 -- Telescope
 ----------------------------------------------------------------------
 -- Fernのキーバインドを設定する
-vim.keymap.set("n", "<Leader>ft", ":Fern . -drawer<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>ft", ":Fern . -drawer -width=50<CR>", { noremap = true, silent = true })
 
 -- Telescopeの設定を行う
 local telescope = require("telescope")
@@ -376,8 +376,34 @@ vim.lsp.config("rust_analyzer", {
 
 vim.lsp.config("fsautocomplete", {})
 
+
+local schemastore = require("schemastore")
+
+vim.lsp.config("jsonls", {
+  settings = {
+    json = {
+      schemas = schemastore.json.schemas(),
+      validate = { enable = true },
+    },
+  },
+})
+
 vim.lsp.config("yamlls", {
-  -- TODO:
+  settings = {
+    yaml = {
+      schemaStore = {
+        -- b0o/schemastore.nvimを使うため
+        enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = "",
+      },
+      schemas = schemastore.yaml.schemas(),
+      -- format = { enable = true },
+      completion = true,
+      hover = true,
+      validate = true,
+    },
+  },
 })
 
 vim.lsp.enable({
@@ -564,6 +590,44 @@ require("nvim-treesitter.configs").setup {
         ['@function.outer'] = 'V',   -- linewise
         ['@class.outer'] = '<c-v>',  -- blockwise
       },
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = { query = "@class.outer", desc = "Next class start" },
+        --
+        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+        ["]o"] = "@loop.*",
+        -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+        --
+        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+        ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+        ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+      -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      goto_next = {
+        ["]d"] = "@conditional.outer",
+      },
+      goto_previous = {
+        ["[d"] = "@conditional.outer",
+      }
     },
   },
 }
