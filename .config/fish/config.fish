fb8d02f2458346b003657e96a0620991a09b07f2
watanany <s1170087@gmail.com>
2026-04-22 14:25:23 +0900

    chore: tidy up config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,60 @@
 if status is-interactive
+    #;;; !$
+    source $(status dirname)/bind.fish
+
+    if type -q nvim
+        set -gx EDITOR nvim
+    else
+        set -gx EDITOR vim
+    end
+
+    set -gx PATH "$HOME/.local/bin" $PATH
+    set -gx PATH "$HOME/.git-subcommands" $PATH
+
+    #;;; エイリアス
+    alias mv="mv -i"
+    alias cp="cp -i"
+    alias curl='curl -sSL'
+    alias ls="ls -F --color=auto"
+    alias fennel="rlwrap fennel"
+
+    function cd
+        builtin cd $argv && ls
+    end
+
+    #;;; brew
     switch $(uname -s):$(uname -p)
         case "Darwin:arm"
             eval $(/opt/homebrew/bin/brew shellenv)
     end
 
-    set -gx EDITOR nvim
-    set -gx FZF_DEFAULT_OPTS "--layout=reverse"
+    #;;; emacs
+    if type -q emacs
+        set -gx PATH "$HOME/.emacs.d/bin" $PATH
+    end
 
-    set -gx PATH "$HOME/.emacs.d/bin" $PATH
-    set -gx PATH "$HOME/.local/bin" $PATH
-    set -gx PATH "$HOME/.git-subcommands" $PATH
+    #;;; fzf
+    if type -q fzf
+        set -gx FZF_DEFAULT_OPTS "--layout=reverse"
+        fzf --fish | source
+    end
 
+    #;;; zoxide
+    if type -q zoxide
+        zoxide init fish | source
+    end
+
+    #;;; direnv
+    if type -q direnv
+        direnv hook fish | source
+    end
+
+    #;;; uv
     if type -q uv
         uv generate-shell-completion fish | source
     end
 
+    #;;; mise
     if type -q mise
         source $(mise activate fish | psub)
     end
@@ -30,8 +70,20 @@ if status is-interactive
     #;;; Rust
     set -gx PATH "$HOME/.cargo/bin" $PATH
 
+    #;;; Bun
+    set -gx BUN_INSTALL "$HOME/.bun"
+    set -gx PATH "$BUN_INSTALL/bin" $PATH
+
+    #;;; Lean 4
+    set -gx PATH "$HOME/.elan/bin" $PATH
+
     #;;; F#
     set -gx PATH "$HOME/.dotnet/tools" $PATH
+
+    #;;; Ocaml
+    if test -r "$HOME/.opam/opam-init/init.fish"
+        source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null
+    end
 
     #;;; Dart/Flutter
     #;; Flutter
@@ -41,34 +93,25 @@ if status is-interactive
     #;; adbなどのコマンドを使えるようにする
     switch $(uname)
         case Darwin
-            set -gx PATH "$HOME/Library/Android/sdk/platform-tools" $PATH
+            if test -d "$HOME/Library/Android/sdk/platform-tools"
+                set -gx PATH "$HOME/Library/Android/sdk/platform-tools" $PATH
+            end
     end
 
     #;;; Android emulator
     switch $(uname)
         case Darwin
-            set -gx PATH "$HOME/Library/Android/sdk/emulator" $PATH
+            if test -d "$HOME/Library/Android/sdk/emulator"
+                set -gx PATH "$HOME/Library/Android/sdk/emulator" $PATH
+            end
     end
 
     #;;; Inkscape command
     switch $(uname)
         case Darwin
-            set -gx PATH "/Applications/Inkscape.app/Contents/MacOS" $PATH
-    end
-
-    #;;; GNU tools
-    switch $(uname)
-        case Darwin
-            set -gx PATH /opt/homebrew/opt/coreutils/libexec/gnubin $PATH
-            set -gx PATH /opt/homebrew/opt/findutils/libexec/gnubin $PATH
-            set -gx PATH /opt/homebrew/opt/gnu-sed/libexec/gnubin $PATH
-            set -gx PATH /opt/homebrew/opt/gnu-tar/libexec/gnubin $PATH
-            set -gx PATH /opt/homebrew/opt/grep/libexec/gnubin $PATH
-            set -gx MANPATH /opt/homebrew/opt/coreutils/libexec/gnuman $MANPATH
-            set -gx MANPATH /opt/homebrew/opt/findutils/libexec/gnuman $MANPATH
-            set -gx MANPATH /opt/homebrew/opt/gnu-sed/libexec/gnuman $MANPATH
-            set -gx MANPATH /opt/homebrew/opt/gnu-tar/libexec/gnuman $MANPATH
-            set -gx MANPATH /opt/homebrew/opt/grep/libexec/gnuman $MANPATH
+            if test -d "/Applications/Inkscape.app/Contents/MacOS"
+                set -gx PATH "/Applications/Inkscape.app/Contents/MacOS" $PATH
+            end
     end
 
     #;;; MySQL
@@ -77,71 +120,18 @@ if status is-interactive
     end
 
     #;;; Google Cloud SDK
-    set -gx PATH "$HOME/.local/google-cloud-sdk/bin" $PATH
+    if test -d "$HOME/.local/google-cloud-sdk/bin"
+        set -gx PATH "$HOME/.local/google-cloud-sdk/bin" $PATH
+    end
+
     if test -f "$HOME/.google-cloud-sdk/path.fish.inc"
         source "$HOME/.google-cloud-sdk/path.fish.inc"
     end
 
-    #;;; Embulk
-    set -gx PATH "$HOME/.embulk/bin" $PATH
-
-    #;;; konryu & cotowali
-    set -gx PATH "$HOME/.konryu/bin" $PATH
-
-    #;;; direnv
-    if type -q direnv
-        direnv hook fish | source
-    end
-
-    #;;; Bun
-    set -gx BUN_INSTALL "$HOME/.bun"
-    set -gx PATH $BUN_INSTALL/bin $PATH
-
-    #;;; Modular
-    set -gx MODULAR_HOME "$HOME/.modular"
-    set -gx PATH $MODULAR_HOME/pkg/packages.modular.com_mojo/bin $PATH
-
-    #;;; Lean 4
-    set -gx PATH $HOME/.elan/bin $PATH
-
-    #;;; エイリアス
-    alias mv="mv -i"
-    alias cp="cp -i"
-    alias curl='curl -sSL'
-    alias ls="ls -F --color=auto"
-
-    function cd
-        builtin cd $argv && ls
-    end
-
-    source $(status dirname)/bind.fish
-
-    #;;; fzf
-    if type -q fzf
-        fzf --fish | source
-    end
-
-    # BEGIN opam configuration
-    # This is useful if you're using opam as it adds:
-    #   - the correct directories to the PATH
-    #   - auto-completion for the opam binary
-    # This section can be safely removed at any time if needed.
-    if test -r "$HOME/.opam/opam-init/init.fish"
-        source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null
-    end
-    # END opam configuration
-
-    alias fennel="rlwrap fennel"
-
-    set -gx PATH "$HOME/.claude/local" $PATH
-
-    switch $(uname)
-        case Darwin
-            alias snowsql="/Applications/SnowSQL.app/Contents/MacOS/snowsql"
-    end
+    #;;; Rancher Desktop
+    set -gx PATH "$HOME/.rd/bin" $PATH
 end
 
 if test -f $(status dirname)/local_config.fish
     source $(status dirname)/local_config.fish
 end
-
